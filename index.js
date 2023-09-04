2251655ff0fa5864f6a26c46133cd75d245f6938
Boda Gábor <121058339+gaborbodaboduss@users.noreply.github.com>
2023-09-04 16:05:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const btnEl = document.getElementById("btn");
+const bmiInputEl = document.getElementById("bmi-result");
+const weightConditionEl = document.getElementById("weight-condition");
+
+function calculateBMI() {
+    const heightValue = document.getElementById("height").value / 100;
+    const weightValue = document.getElementById("weight").value;
+
+    const bmiValue = weightValue / (heightValue * heightValue);
+
+    bmiInputEl.value = bmiValue
+    /* console.log(heightValue, weightValue); */
+
+    if(bmiValue < 18.5){
+        weightConditionEl.innerText = "Under weight ";
+    } else if(bmiValue >= 18.6 && bmiValue <= 24.9) {
+        weightConditionEl.innerText = "Normal weight";
+    } else if(bmiValue >= 25 && bmiValue <= 29.9) {
+        weightConditionEl.innerText = "Overweight";
+    } else if(bmiValue >= 30) {
+        weightConditionEl.innerText ="Obesed";
+    }
+
+
+}
+
+
+
+btnEl.addEventListener("click", calculateBMI)
